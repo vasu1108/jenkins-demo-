1513e754e2b49fe1235f60d0f1b813f15e292398
vasu1108 <shreenivasan1108@gmail.com>
2025-04-02 16:23:04 +0530

    first jenkins commit

---
+++ src/main/java/com/techm/jenkinsdemo/App.java
@@ -0,0 +1,13 @@
+package com.techm.jenkinsdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
